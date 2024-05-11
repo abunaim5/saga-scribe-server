ce50200c792b1ee084f490d1d7f944f63354eee7
abunaim5 <abunaim140@gmail.com>
2024-05-11 21:31:49 +0600

    implement find blogs data by create blogs api

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const blogsCollection = client.db('sagaScribeDB').collection('blogs');
+
+    app.get('/blogs', async (req, res) => {
+        const result = await blogsCollection.find().toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
