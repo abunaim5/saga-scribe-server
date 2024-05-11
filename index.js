dbe727f10f34dd87624155efbd28081471ade330
abunaim5 <abunaim140@gmail.com>
2024-05-11 23:38:12 +0600

    implement find single blog data with specific id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,7 +32,14 @@ async function run() {
     app.get('/blogs', async (req, res) => {
         const result = await blogsCollection.find().toArray();
         res.send(result);
-    })
+    });
+
+    app.get('/blogs/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await blogsCollection.findOne(query);
+        res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
